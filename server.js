9b3baac2cff14d699c78956f7a431ba8ab2d4800
Francois Laberge <francoislaberge@gmail.com>
2011-08-26 22:51:10 -0700

    Deployed using jitsu now

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
-var http = require('http'), 
-	nko = require('nko')('2tDiu6H8P/Y44dI1');
+var http = require('http');
 
-var app = http.createServer(function (req, res) { 
-	res.writeHead(200, { 'Content-Type': 'text/html' }); 
-	res.end('Hello, World'); 
+//
+// START NODE KNOCKOUT TRACKING CODE
+//
+
+//
+// Remark: This code MUST be required ONCE somewhere in your application for,
+// node knockout tracking to work.
+//
+require('./vendor/nko')('2tDiu6H8P/Y44dI1', function(err, res) {
+  if (err) {
+    console.error('Warning: Running in development mode.');
+    //console.error(err.stack);
+  }
 });
 
-app.listen(parseInt(process.env.PORT) || 7777); 
-console.log('Listening on ' + app.address().port);
+//
+//  END NODE KNOCKOUT TRACKING CODE
+//
+
+
+//
+// Start up an http server
+//
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('<h1>I\'m gonna node knock you out.</h1>');
+}).listen(8080);
